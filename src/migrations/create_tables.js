9347b9f536f2246a49fd71e9c9b026e46cc62c3a
Charles NDAYISABA <nccharles1@gmail.com>
2022-01-07 01:42:32 +0200

    redefining DB

--- src/migrations/create_tables.js
+++ src/migrations/create_tables.js
@@ -1,16 +1,16 @@
 import { DB } from "../config/database";
-import { Pool } from 'pg';
+import { Pool } from "pg";
 const pool = new Pool({
-  connectionString: DB
+  connectionString: DB,
 });
 
 const dropping = async () => {
   const userMigration = `DROP TABLE IF EXISTS users CASCADE`;
-  const categoryMigration = `DROP TABLE IF EXISTS category CASCADE`;
+  const issueMigration = `DROP TABLE IF EXISTS issue CASCADE`;
   try {
     await pool.query(userMigration);
-    await pool.query(categoryMigration);
-    console.log('Tables dropped');
+    await pool.query(issueMigration);
+    console.log("Tables dropped");
   } catch (err) {
     console.log(`${err}, Dropped failed`);
   }
@@ -18,15 +18,14 @@ const dropping = async () => {
 const insertData = async () => {
   const AdminInsert = `INSERT INTO users(first_name,last_name,email,address,password,phoneNumber,isAdmin) 
   VALUES('Charles','NDAYISABA','charles@tracker.rw','Kigali','$2a$08$jXozHrKh0B.DZJ9jvGO3IeMRwk9gT.5T2kfOEs0MGI6t4WrE3lDS6','0785856892','1')`;
- 
+
   try {
     await pool.query(AdminInsert);
-    await pool.query(categoryInsert);
-    console.log('Data Inserted');
+    console.log("Data Inserted");
   } catch (err) {
     console.log(`${err}, Inserted failed`);
   }
-}
+};
 const usersTable = `CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY NOT NULL,
     first_name VARCHAR(80) NOT NULL,
@@ -37,11 +36,18 @@ const usersTable = `CREATE TABLE IF NOT EXISTS users(
     phoneNumber VARCHAR(10) NOT NULL,
     isAdmin INTEGER DEFAULT 3
   );`;
-
+const issuesTable = `CREATE TABLE IF NOT EXISTS issues(
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(80) NOT NULL,
+    description VARCHAR(80) NOT NULL,
+    reporter INTEGER DEFAULT 1,
+    screenshot VARCHAR(500) NOT NULL
+  );`;
 const createAllTables = async () => {
   try {
     await pool.query(usersTable);
-    console.log('created')
+    await pool.query(issuesTable);
+    console.log("created");
   } catch (err) {
     console.log(`creation failed`);
   }
@@ -54,4 +60,3 @@ const trackerTables = async () => {
   await process.exit(0);
 };
 trackerTables();
-
